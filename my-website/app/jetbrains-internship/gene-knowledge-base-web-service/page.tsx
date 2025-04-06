c4023bdba59bbd61846402f11c365e1d8e105a30
heidi albarazi <heidi.albarazi@tum.de>
2025-04-06 16:34:10 +0200

    add jetbrains internship pages

---
+++ my-website/app/jetbrains-internship/gene-knowledge-base-web-service/page.tsx
@@ -0,0 +1,15 @@
+import { title } from "@/components/primitives";
+import React from "react";
+import Image from "next/image";
+import {Timeline} from "@/components/ui/timeline";
+
+export default function GeneKnowledgeBaseWebServicePage() {
+
+  return (
+    <div>
+      <h1 className={title()}>Gene Knowledge Base Web Service Internship Application</h1>
+
+
+    </div>
+  );
+}
---
+++ my-website/app/jetbrains-internship/growth-and-community/page.tsx
@@ -0,0 +1,15 @@
+import { title } from "@/components/primitives";
+import React from "react";
+import Image from "next/image";
+import {Timeline} from "@/components/ui/timeline";
+
+export default function GrowthAndCommunityWebServicePage() {
+
+  return (
+    <div>
+      <h1 className={title()}>Growth and Community Internship</h1>
+
+
+    </div>
+  );
+}
